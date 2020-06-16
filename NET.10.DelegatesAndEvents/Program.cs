63feadbcc3c16dcdf2e2779e16c6b68222767959
Игорь Соколов <strelok111@gmail.com>
2020-06-16 22:45:39 +0300

    New templates for classes

---
+++ NET.10.DelegatesAndEvents/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace NET._10.DelegatesAndEvents
+{
+    class Program
+    {
+        static Timer TheTimer;
+        static void Main(string[] args)
+        {
+
+            GenerateTimer(out string timerName, out int timerValue);
+
+            TheTimer = new Timer(timerName, timerValue);
+            TheTimer.TimerEvent += new TimerDelegate(TimerReciever);
+            TheTimer.CountDown += new TimerDelegate(TimerCount);
+            TheTimer.Start();
+
+            Console.ReadKey();
+            TheTimer.ToString();
+        }
+
+        private static void TimerReciever(object sender, TimerEventArgs e)
+        {
+            MessageBox.Show($"Timer '{ e.Name }' завершен за { e.Delay / 1000 } с.");
+        }
+        
+        private static void TimerCount(object sender, TimerEventArgs e)
+        {
+            Console.WriteLine($"Timer '{ e.Name }' - { e.RemainTime } с." );
+        }
+
+        public static void GenerateTimer(out string timerName, out int timerValue)
+        {
+            Console.WriteLine("Введите имя таймера: ");
+            timerName = Console.ReadLine();
+            // Время в мс
+            timerValue = GetNumericInput("Введите время, с: ") * 1000;
+        }
+
+        private static int GetNumericInput(string text)
+        {
+            Console.WriteLine(text);
+            bool success = Int32.TryParse(Console.ReadLine(), out int parsed);
+            if (success)
+            {
+                return parsed;
+            }
+            else
+            {
+                Console.WriteLine("Число не введено.");
+                return 0;
+            }
+        }
+
+        private static void FeedbackToConsole(Int32 value)
+        {
+            Console.WriteLine("Item=" + value);
+        }
+
+        private static void FeedbackToMsgBox(Int32 value)
+        {
+            MessageBox.Show("Item=" + value);
+        }
+
+        private void FeedbackToFile(Int32 value)
+        {
+            StreamWriter sw = new StreamWriter("Status", true);
+            sw.WriteLine("Item=" + value);
+            sw.Close();
+        }
+    }
+}
---
+++ NET.10.DelegatesAndEvents/Timer.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+
+namespace NET._10.DelegatesAndEvents
+{
+    public delegate void TimerDelegate(object sender, TimerEventArgs e);
+
+    public class TimerEventArgs : EventArgs
+    {
+        public TimerEventArgs(string name, int delay) : base()
+        {
+            this.Name = name;
+            this.Delay = delay;
+        }
+
+        public TimerEventArgs(string name, int delay, int remainTime) : this(name, delay)
+        {
+            this.RemainTime = remainTime;
+        }
+
+        public string Name { get; private set; }
+        public int Delay { get; private set; }
+        public int RemainTime { get; private set; }
+
+
+    }
+
+    class Timer
+    {
+        public event TimerDelegate TimerEvent;
+        public event TimerDelegate CountDown;
+
+        string name;
+        int delay;
+        
+
+        public Timer(string Name, int Delay)
+        {
+            this.name = Name;
+            this.delay = Delay;
+        }
+
+        public void Start()
+        {
+            Thread thread = new Thread(new ThreadStart(() => {
+
+                var sw = Stopwatch.StartNew();               
+                do
+                {
+                    //Console.WriteLine("Thread {0}: Elapsed {1:N2} seconds",
+                    //                  Thread.CurrentThread.ManagedThreadId,
+                    //                  sw.ElapsedMilliseconds / 1000.0);
+                    OnCountDown(this, new TimerEventArgs(name, delay, (delay - (int)sw.ElapsedMilliseconds) / 1000));
+                    Thread.Sleep(900);
+                } while (sw.ElapsedMilliseconds <= delay);
+                sw.Stop();
+
+                OnTimer(this, new TimerEventArgs(name, delay));
+            }));
+            thread.Start();
+        }
+
+        protected virtual void OnTimer(object sender, TimerEventArgs e)
+        {
+            TimerEvent?.Invoke(sender, e);
+        }
+
+        protected virtual void OnCountDown(object sender, TimerEventArgs e)
+        {
+            CountDown?.Invoke(sender, e);
+        }
+    }
+}
